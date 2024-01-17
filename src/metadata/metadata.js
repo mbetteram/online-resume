38311c65ee4165858e8f884d3c7d2987b5c48417
mbetteram <bettera.marcat@gmail.com>
2024-01-16 21:51:08 -0300

    cv upload

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -13,6 +13,7 @@ module.exports = {
     ['Fortran', 90],
     ['C', 70],
     ['Bash/Shell scripting', 80],
+    ['ssh', 90],
     ['gnuplot', 90],
     ['Python', 90],
     ['numpy', 100],
@@ -22,7 +23,6 @@ module.exports = {
     ['Git/Github', 80],
     ['scikit-learn', 80],
     ['tensorflow', 70],
-    ['ssh', 90],
     ['SQL', 70]
   ],
 
@@ -37,13 +37,15 @@ module.exports = {
       contents: `
 [Universidad Empresarial Siglo 21](https://21.edu.ar/) is a bussiness oriented private university. I am proffessor of theoretical and poblem clases of many mathematics courses, statcis and physics.
 
-  -__ Distance courses _(two months period)_: Physics, Statics, Mathematical Analisys, Algebra.
-  -__ Presential intensive courses _(two months period)_: Mathematical Analisys, Statistics II.
-  -__ Presential courses _(four months period)_: Mathematical Analisys.
+  -__Distance courses__ _(two months period)_: Physics, Statics, Mathematical Analisys, Algebra.
+
+  -__Presential intensive courses__ _(two months period)_: Mathematical Analisys, Statistics II.
+
+  -__Presential courses__ _(four months period)_: Mathematical Analisys.
 `
     },
     {
-      company: 'Data Science Diploma. Mathematics, Astronomy, Physics and Computer Science school, National University of Córdoba. _(FAMAF, UNC)_',
+      company: 'Data Science Diploma. Mathematics, Astronomy, Physics and Computer Science school, National University of Córdoba. (FAMAF, UNC)',
       location: 'Cordoba, AR',
       title: 'Monitoring teacher',
       period: 'May 2023 - Sep 2023',
@@ -53,37 +55,37 @@ My role at [Data science diploma, at FAMAF-UNC](https://diplodatos.famaf.unc.edu
 `
     },
     {
-      company: 'National University of Rio Cuarto, Argentina. _(UNRC)_',
+      company: 'National University of Rio Cuarto, Argentina. (UNRC)',
       location: 'Rio Cuarto, AR',
-      title: 'Lecturer _(Profesor Ayudante de Priemra.)_',
+      title: 'Lecturer (Profesor Ayudante de Primera.)',
       period: 'Mar 2022 – Present, Mar 2016 - Jul 2016',
       // skills: ['AWS', 'Terraform', 'NodeJS', 'GraphQL', 'React', 'Mobx', 'Redux', 'Webpack', 'Jest', 'Storybook'],
       contents: `
 Lecturer for problem classes. Evaluation of midterm and final examns.
 
-- __2nd sem 23__ Thermodynamics and Statistical Mechanics II _and_ Modern Physics I
-- __1st sem 23__ Thermodynamics and Statistical Mechanics I _and_ Physics 4 (Modern Physics)
-- __2nd sem 22__ General Physics III (Electricity and Magnetism).
-- __1st sem 22__ Thermodynamics and Statistical Mechanics I.
-- __1st sem 16__ General Physics IV (Waves and Optics).
+- __2nd term 23__ Thermodynamics and Statistical Mechanics II _and_ Modern Physics I
+- __1st term 23__ Thermodynamics and Statistical Mechanics I _and_ Physics 4 (Modern Physics)
+- __2nd term 22__ General Physics III (Electricity and Magnetism).
+- __1st term 22__ Thermodynamics and Statistical Mechanics I.
+- __1st term 16__ General Physics IV (Waves and Optics).
 `,
     },
     {
-      company: 'Mathematics, Astronomy, Physics and Computer Science school, National University of Córdoba, Argentina. _(FAMAF, UNC)_',
+      company: 'Mathematics, Astronomy, Physics and Computer Science school, National University of Córdoba, Argentina. (FAMAF, UNC)',
       location: 'Cordoba, AR',
-      title: 'Lecturer _(Profesor Ayudante B D.S.)_',
+      title: 'Lecturer (Profesor Ayudante B D.S.)',
       period: 'March 2022 – Feb 2023',
       // skills: ['AWS', 'Terraform', 'NodeJS', 'GraphQL', 'React', 'Mobx', 'Redux', 'Webpack', 'Jest', 'Storybook'],
       contents: `
 Lecturer for problem classes. Evaluation of midterm and final examns.
  
   - __Feb 22__ Admission and introductory course.  
-  - __2nd sem. 22__ Mechanics
-  - __1st sem. 22__ Admission and introductory course.
+  - __2nd term 22__ Mechanics
+  - __1st term 22__ Admission and introductory course.
 `,
     },
     {
-      company: 'CONICET _(Argentinian institution for research public funding)_',
+      company: 'CONICET (Argentinian institution for research public funding)',
       location: 'Cordoba, AR, NL',
       title: 'Research',
       period: 'Apr 2014 – Feb 2017',
@@ -97,17 +99,17 @@ _(Investigación sobre materia activa, en particular dinámica de espermátozoid
 `,
     },
     {
-      company: 'National University of Rio Cuarto, Argentina. _(UNRC)_',
+      company: 'National University of Rio Cuarto, Argentina. (UNRC)',
       location: 'Rio Cuarto, AR',
-      title: 'Student assistant _(Alumno Ayudante de Segunda D.S.)_',
+      title: 'Student assistant (Alumno Ayudante de Segunda D.S.)',
       period: 'Aug 2014 – Jul 2015, Aug 2012 – Jul 2013',
       // skills: ['AWS', 'Terraform', 'NodeJS', 'GraphQL', 'React', 'Mobx', 'Redux', 'Webpack', 'Jest', 'Storybook'],
       contents: `
 Student assistant for problem classes.
-  - __1st sem 15__ Electromagnetism I
-  - __2nd sem 14__ General Physics I (Introduction to Mechanics)
-  - __1st sem 13__ Mechanics
-  - __2nd sem 12__ Electromagnetism I
+  - __1st term 15__ Electromagnetism I
+  - __2nd term 14__ General Physics I (Introduction to Mechanics)
+  - __1st term 13__ Mechanics
+  - __2nd term 12__ Electromagnetism I
 `,
     },
   ],
